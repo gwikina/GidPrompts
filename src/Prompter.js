1e491f307045160474985d647d9e2ce05fe0056a
Gideon Wikina <widdy49@gmail.com>
2022-05-17 01:57:41 -0400

    added components to the prompt

--- src/Prompter.js
+++ src/Prompter.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Component } from 'react'
 import {Container, Form, Button, Card} from 'react-bootstrap'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 const {Configuration, OpenAIApi} = require("openai");
 
 
@@ -27,7 +27,45 @@ class Prompter extends Component{
     render(){
         return(
             <div>
-                hello
+                <Container>
+                    <br/>
+                    <br/>
+                    <h1> Generate Responses To Prompts </h1>
+                    <br />
+                    <h4> Enter a prompt and OpenAI will give you a response</h4>
+                    <br />
+                    <br />
+                    <Form  onSubmit={this.onFormSubmit}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label> Enter your prompt </Form.Label>
+                            <Form.Control
+                                        type="text"
+                                        name="prompt"
+                                        placeholder="Enter a prompt for OpenAI"
+                            />
+                            <Form.Text>
+                                Enter as much information as possible for best answers
+                            </Form.Text>
+                        </Form.Group>
+                        <Button variant="primary" type="lg" type="submit">
+                                Get AI Answers
+                        </Button>
+                    </Form>
+                    <br/> 
+                    <br/>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title> <h1> {this.state.heading} </h1> </Card.Title>
+                            <hr/>
+                            <br/>
+                            <Card.Text>
+                                <h4>
+                                {this.state.response}
+                                </h4>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Container>
             </div>
         )
     }
