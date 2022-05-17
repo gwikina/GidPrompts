8699f3509a17a8813b4978cb12643f5659bbe678
Gideon Wikina <widdy49@gmail.com>
2022-05-17 04:33:08 -0400

    Updates

---
+++ precache-manifest.469882bc3a5c9d86039fa54420e794ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ef6301c28d3a39bc6bb",
+    "url": "/GidPrompts/static/css/main.54e30230.chunk.css"
+  },
+  {
+    "revision": "5ef6301c28d3a39bc6bb",
+    "url": "/GidPrompts/static/js/main.5ef6301c.chunk.js"
+  },
+  {
+    "revision": "897aa20ba8584a8a9d67",
+    "url": "/GidPrompts/static/js/1.897aa20b.chunk.js"
+  },
+  {
+    "revision": "b5e718ad8e650a9b4d06",
+    "url": "/GidPrompts/static/css/2.b7f6855d.chunk.css"
+  },
+  {
+    "revision": "b5e718ad8e650a9b4d06",
+    "url": "/GidPrompts/static/js/2.b5e718ad.chunk.js"
+  },
+  {
+    "revision": "a9ccf56c80ecf3619fbe",
+    "url": "/GidPrompts/static/js/runtime~main.a9ccf56c.js"
+  },
+  {
+    "revision": "7199cb1aa3be1588214247ee553a6844",
+    "url": "/GidPrompts/index.html"
+  }
+];
